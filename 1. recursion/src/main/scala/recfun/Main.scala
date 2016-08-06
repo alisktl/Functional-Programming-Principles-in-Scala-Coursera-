78706316419ad41b955f7ce0273ecf7dd97bbe73
alisktl <alisktl@gmail.com>
2016-08-07 01:35:25 +0600

    1st Assignment SOLVED

--- 1. recursion/src/main/scala/recfun/Main.scala
+++ 1. recursion/src/main/scala/recfun/Main.scala
@@ -10,20 +10,62 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("\nBalance: balance(List(4,3))")
+    println(balance(List(4,3)))
+
+    println("\ncountChange: countChange(6, List(3, 1, 2))")
+    println(countChange(6, List(3, 1, 2)))
   }
 
   /**
     * Exercise 1
     */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+
+    def loop(c: Int, r: Int): Int = {
+      if(c < 0 || r < 0) 0
+      else if(c == r) 1
+      else {
+        loop(c, r - 1) + loop(c - 1, r - 1)
+      }
+    }
+
+    loop(c, r)
+  }
 
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    @tailrec
+    def loop(n: Int, chars: List[Char]): Boolean = {
+      var m = n
+      if(chars.isEmpty && n == 0) return true
+      else {
+        if(chars.head == '(') m = n + 1
+        else if(chars.head == ')') m = n - 1
+
+        if(m < 0) return false
+      }
+
+      loop(m, chars.tail)
+    }
+
+    loop(0, chars)
+  }
 
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money < 0 || coins.isEmpty)
+      0
+    else if(money == 0)
+      1
+    else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
