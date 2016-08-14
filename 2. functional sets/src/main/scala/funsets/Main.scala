be87858f5e5e308ff8a9e763d0cb6b4ec93fe9c7
alisktl <alisktl@gmail.com>
2016-08-15 01:34:33 +0600

    2nd Assignment SOLVED

--- 2. functional sets/src/main/scala/funsets/Main.scala
+++ 2. functional sets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,29 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  val x = singletonSet(1)
+  val y = singletonSet(2)
+  val z = singletonSet(3)
+  val w = union(union(x, z), y)
+
+  println("\nx: ")
+  printSet(x)
+
+  println("\ny: ")
+  printSet(y)
+
+  println("\nz: ")
+  printSet(z)
+
+  println("\nw: ")
+  printSet(w)
+
+  println("\nforall(x,w): " + forall(x,w))
+
+  println("\nintersect(w, y): ")
+  printSet(intersect(w, y))
+
+  println("\ndiff(w, y): ")
+  printSet(diff(w, y))
 }
